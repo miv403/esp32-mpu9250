c67863483b7f465d6f7aa8dfd59a9a4f629a0d1a
miv403 <miv403@duck.com>
2025-07-09 16:46:22 +0300

    added try cath to recv-sensor python script

--- send-sensor-data/recv-sensor-data.py
+++ send-sensor-data/recv-sensor-data.py
@@ -7,25 +7,27 @@ ser = serial.Serial('/dev/ttyUSB0', 115200)  # Adjust to your port
 # Struct format: 9 floats, little-endian
 struct_format = '<9f'
 struct_size = struct.calcsize(struct_format)
-
-while True:
-    # Wait for header (0xAA 0x55)
+try: 
     while True:
-        if ser.read() == b'\xAA':
-            if ser.read() == b'\x55':
-                break
-
-    # Read full struct
-    data_bytes = ser.read(struct_size)
-
-    if len(data_bytes) == struct_size:
-        values = struct.unpack(struct_format, data_bytes)
-        # print("Accel:", values[0:3], "Gyro:", values[3:6], "Mag:", values[6:9])
-        gyroRaw  = values[0:3]
-        accelRaw = values[3:6]
-        magRaw   = values[6:9]
-        tempRaw  = values[9:10]
-        print("Gyro: ", gyroRaw )
-        print("Accel: ",  accelRaw)
-        print("Mag: ",  magRaw  )
-        print("Temp: ",  tempRaw )
+        # Wait for header (0xAA 0x55)
+        while True:
+            if ser.read() == b'\xAA':
+                if ser.read() == b'\x55':
+                    break
+    
+        # Read full struct
+        data_bytes = ser.read(struct_size)
+    
+        if len(data_bytes) == struct_size:
+            values = struct.unpack(struct_format, data_bytes)
+            # print("Accel:", values[0:3], "Gyro:", values[3:6], "Mag:", values[6:9])
+            gyroRaw  = values[0:3]
+            accelRaw = values[3:6]
+            magRaw   = values[6:9]
+            tempRaw  = values[9:10]
+            print("Gyro: ", gyroRaw )
+            print("Accel: ",  accelRaw)
+            print("Mag: ",  magRaw  )
+            print("Temp: ",  tempRaw )
+except KeyboardInterrupt:
+    exit()
