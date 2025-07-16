e156f13fd3b210e4a8f1dc7a6637e51f32bea84a
miv403 <miv403@duck.com>
2025-07-16 15:51:35 +0300

    added some functions and changes on sensor data send functions
    loop():
       i've fixed the sensor data sending behaviour.
    
    getBias():
        gets bias with mpu.getBias() functions
        sets calibrationData struct
    setBias():
        i didn't impelement it yet
        it sets bias with saved calibration data
        then sets offset registers using mpu.h functions

--- serial-comm/serial-comm.ino
+++ serial-comm/serial-comm.ino
@@ -5,23 +5,24 @@
 #define CMD_RECV_CALIB  0x02
 #define CMD_GET_SENSOR  0x03
 
-#define RESP_OK           0xAA
-#define RESP_ERR          0xFF
 #define RESP_CALIB_DONE   0x10
 #define RESP_CALIB_RCVD   0x11
 #define RESP_SENSOR_MODE  0x12
 #define RESP_SENSOR_SENT  0x13
+#define RESP_OK           0xAA
+#define RESP_ERR          0xFF
 
 MPU9250 mpu;
 
+// struct order: acc, gyro
 struct CalibrationData {
   float accBias[3];
   float gyroBias[3];
 } calib;
 
 struct SensorData {
-  float gyro[3];
   float acc[3];
+  float gyro[3];
 }sensorDat;
 
 void setup() {
@@ -34,61 +35,42 @@ void setup() {
     if (Serial.available()) {
       uint8_t cmd = Serial.read();
 
-      if        (cmd == CMD_CALIBRATE) {
+      if (cmd == CMD_CALIBRATE) {
         Serial.write(RESP_OK);
         calibrate();
-        Serial.write(RESP_CALIB_DONE);
+        //Serial.write(RESP_CALIB_DONE);
 
         // Serial.write(0xAA);
         // Serial.write(0x55);
 
         Serial.write((uint8_t*)&calib, sizeof(CalibrationData));
       } else if (cmd == CMD_RECV_CALIB) {
-        while   (Serial.available() < sizeof(CalibrationData));
+        // gets saved calibration data from serial
+        Serial.write(RESP_OK);
+
+        while (Serial.available() < sizeof(CalibrationData));
         Serial.readBytes( (char*)&calib, sizeof(CalibrationData));
+        Serial.write(RESP_CALIB_RCVD);
       } else if (cmd == CMD_GET_SENSOR) {
         start_loop = true;
       }
     }
   }
+  Serial.write(RESP_SENSOR_MODE);
 }
+
 
 void loop() {
-  if (Serial.available()) {
-    uint8_t cmd = Serial.read();
+    setSensorData(); // gets & sets sensorData before send
 
-    if (cmd == CMD_GET_SENSOR) {
-      float data[9] = {
-        mpu.getAccX(), mpu.getAccY(), mpu.getAccZ(),
-        mpu.getGyroX(), mpu.getGyroY(), mpu.getGyroZ(),
-        mpu.getMagX(), mpu.getMagY(), mpu.getMagZ()
-      };
-      Serial.write((byte*)data, sizeof(data));
-    }
-  }
+    //Serial.write(0xAA); // header to read properly
+    //Serial.write(0x55);
+
+    Serial.write((uint8_t*)&sensorDat, sizeof(sensorDat));
+    delay(500);
 }
 
-void calibrate() {
-
-    // calibrate anytime you want to
-    // Serial.println("Accel Gyro calibration will start in 5sec.");
-    // Serial.println("Please leave the device still on the flat plane.");
-    mpu.verbose(true);
-    // delay(3000);
-    mpu.calibrateAccelGyro();
-
-    // Serial.println("Mag calibration will start in 5sec.");
-    // Serial.println("Please Wave device in a figure eight until done.");
-    // delay(3000);
-    // mpu.calibrateMag();
-
-    setCalibrationData();
-
-    // print_calibration();
-    mpu.verbose(false);
-}
-
-void setCalibrationData() {
+void getBias() {
   calib.accBias[0]  = mpu.getAccBiasX();
   calib.accBias[1]  = mpu.getAccBiasY();
   calib.accBias[2]  = mpu.getAccBiasZ();
@@ -98,8 +80,49 @@ void setCalibrationData() {
   calib.gyroBias[2] = mpu.getGyroBiasZ();
 }
 
+void setBias() { // sets mpu9250's offset registers
+  mpu.setAccBias (calib.accBias[0],
+                  calib.accBias[1],
+                  calib.accBias[2]);
+
+  mpu.setGyroBias(calib.gyroBias[0],
+                  calib.gyroBias[1],
+                  calib.gyroBias[2]);
+}
+
+void calibrate() {
+
+    // calibrate anytime you want to
+    // Serial.println("Accel Gyro calibration will start in 5sec.");
+    // Serial.println("Please leave the device still on the flat plane.");
+    mpu.verbose(true);
+    // delay(2000);
+    mpu.calibrateAccelGyro();
+
+    // Serial.println("Mag calibration will start in 5sec.");
+    // Serial.println("Please Wave device in a figure eight until done.");
+    // delay(3000);
+    // mpu.calibrateMag();
+
+    getBias(); // saves the acquired bias to CalibrationData struct
+
+    // print_calibration(); // old function to print biases to serial
+    mpu.verbose(false);
+}
+
+void setSensorData() {
+    // the current sensor data should be packaged before transmission
+
+    sensorDat.acc[0] = mpu.getAccX();
+    sensorDat.acc[1] = mpu.getAccY();
+    sensorDat.acc[2] = mpu.getAccZ();
+
+    sensorDat.gyro[0] = mpu.getGyroX();
+    sensorDat.gyro[1] = mpu.getGyroY();
+    sensorDat.gyro[2] = mpu.getGyroZ();
+}
+
 /*
- * 
   if(Serial.available) {
     sensorDat.gyro[0] = mpu.getGyroX();
     sensorDat.gyro[1] = mpu.getGyroY();
@@ -113,4 +136,13 @@ void setCalibrationData() {
 
     Serial.write((uint8_t*)&sensorDat, sizeof(sensorDat));
   }
+
+    if (cmd == CMD_GET_SENSOR) {
+      float data[9] = {
+        mpu.getAccX(), mpu.getAccY(), mpu.getAccZ(),
+        mpu.getGyroX(), mpu.getGyroY(), mpu.getGyroZ(),
+        mpu.getMagX(), mpu.getMagY(), mpu.getMagZ()
+      };
+      Serial.write((byte*)data, sizeof(data));
+    }
  */
